f829f3612e17f883f4bbded357961fe26f475262
dcelisgarza <daniel.celisgarza@ox.ac.uk>
2016-11-03 16:44:17 +0000

    finshed comp tech code

--- comp_techniques/assignment2.py
+++ comp_techniques/assignment2.py
@@ -9,38 +9,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sc
 
+# Sheath object for easy generalisation.
 class sheath(object):
     
-    def __init__(self, f0, vs, mi = 1840, me = 1):
+    # Initialisation subroutine.
+    def __init__(self, f0, vs, mi = 1840., me = 1.):
         # Initialise f0 with the values of the starting conditions.
-        self.f0 = [f0[i] for i in range(len(f0))]      
+        # f0 = [phi_initial, E_initial]
+        self.f0 = [f0[i] for i in range(len(f0))]    
+        # vs = initial ion velocity
         self.vs  = vs
+        # vs2 = vs^2
         self.vs2 = vs*vs
-        self.cnt = np.sqrt(mi/(2*np.pi*me))
+        # Exponential constant, mi and me take defaults if not given.
+        self.cnt = np.sqrt(mi/(2.*np.pi*me))
     
+    # Checking initialised values.
     def show(self):
         print(self.f0)
         print(self.vs)
         print(self.vs2)
         print(self.cnt)
     
+    # Subroutine called by the integrator.
     def __call__(self, f, x):
+        # Electrostatic potential.
         phi = f[0]
+        # Electric field.
         e   = f[1]
-        vi  = np.sqrt(self.vs2 - 2*phi)
-        dphidx    = -e
-        nd2phidx2 = self.vs/vi - np.exp(phi)
-        return [dphidx, nd2phidx2]
+        # Ion velocity.
+        vi  = np.sqrt(self.vs2 - 2.*phi)
+        
+        dphidx = -e
+        # dedx = -(d^2/dx^2) * phi
+        dedx   = self.vs/vi - np.exp(phi)
+        return [dphidx, dedx]
     
+    # Calculate the current.
     def current(self, phi):
-        return self.cnt * np.exp(phi) - 1
+        return self.cnt * np.exp(phi) - 1.
 
 def run(system):
     
-    def grids(alpha, minor = 1): # Grid function.
+    # Grid function.
+    def grids(alpha, minor = 0): 
         plt.grid(b = True, which = "major", linestyle = "--", alpha = alpha)
-        if minor == 1:
-            plt.grid(b = True, which = "minor", linestyle = "-.", alpha = alpha/4)
+        if minor != 0:
+            plt.grid(b = True, which = "minor", linestyle = "-.", alpha = alpha/4.)
     
     # Close all figures.
     plt.close("all")
@@ -48,25 +63,41 @@ def run(system):
     plt.rc("text", usetex = True)
     plt.rc("font", family = "serif", size = 16)
     
-    plt.figure(1) # Create figure.
-    x = np.linspace(0., 40., 100) # Create linspace.
-    for vs in [1.0, 1.5, 2.0]: # Loop over vs
-        system = sheath([0., 0.001], vs) # Initialise system with desired vs.
-        f = sc.integrate.odeint(system, system.f0, x) # Integrate
-        j  = system.cnt * np.exp(f[:,0]) - 1 # Calculate J
-        x = x - np.interp(0., j[::-1], x[::-1]) # Interpolate to find x intersect.
-        plt.plot(x, j, label = (r"$v_{s} = %.1f$" % system.vs)) # Plot each curve
+    # Create figure.
+    plt.figure(1) 
+    # Linspace for x.
+    x = np.linspace(0., 40., 100) 
+    # vs array.
+    vs_arr = np.linspace(1.,2.,3) 
+    # Loop over vs_arr.
+    for vs in vs_arr:
+        # Initialise system with desired vs.
+        system = sheath([0., 0.001], vs) 
+        # Integrate.
+        f = sc.integrate.odeint(system, system.f0, x)
+        # Calculate J.
+        j  = system.current(f[:,0])
+        # Interpolate to find x when J = 0.
+        x_wall = x - np.interp(0., j[::-1], x[::-1])
+        # Plot each curve.
+        plt.plot(x_wall, j, label = (r"$v_{s} = %.1f$" % system.vs)) 
     
-    grids(0.5, 0.0)
-    plt.ylabel(r"Normalised Current")
-    plt.xlabel(r"Debye Lengths")
+    # Add grids.
+    grids(0.5)
+    # Labels.
+    plt.ylabel(r"$J$, [Normalised]")
+    plt.xlabel(r"$x$, [Debye Lengths]")
+    # Legend position.
     plt.legend(loc=0)
-    plt.tight_layout
+    # Tight layout.
     plt.tight_layout()
+    # Save figure.
     plt.savefig(filename = "changevs.eps", format = "eps")
+    # Show figure.
+    plt.show()
 
-
-system = sheath([0.,0.001], 1.)
+# Initialise system with any values, they're changed inside run.
+system = sheath([0.,0.], 0.)
 run(system)
 
 
--- comp_techniques/assignment3.py
+++ comp_techniques/assignment3.py
@@ -11,14 +11,21 @@ import scipy as sc
 
 class sheath(object):
     
-    def __init__(self, f0, vs, l, mi = 1840, me = 1):
+    # Initialisation subroutine.
+    def __init__(self, f0, vs, l, mi = 1840., me = 1.):
         # Initialise f0 with the values of the starting conditions.
-        self.f0 = [f0[i] for i in range(len(f0))]      
+        # f0 = [phi_initial, E_initial, vi_initial]
+        self.f0 = [f0[i] for i in range(len(f0))]
+        # vs = initial ion velocity
         self.vs  = vs
+        # vs2 = vs^2
         self.vs2 = vs*vs
+        # l = mean collision length
         self.l = l
-        self.cnt = np.sqrt(mi/(2*np.pi*me))
+        # Exponential constant, mi and me take defaults if not given.
+        self.cnt = np.sqrt(mi/(2.*np.pi*me))
     
+    # Checking initialised values.
     def show(self):
         print(self.f0)
         print(self.vs)
@@ -26,23 +33,31 @@ class sheath(object):
         print(self.l)
         print(self.cnt)
     
+    # Subroutine called by the integrator.
     def __call__(self, f, x):
+        # Electrostatic potential.
         phi = f[0]
+        # Electric field.
         e   = f[1]
+        # Ion velocity.
         vi  = f[2]
+        
         dphidx    = -e
-        nd2phidx2 = self.vs/vi - np.exp(phi)
+        # dedx = -(d^2/dx^2) * phi
+        dedx = self.vs/vi - np.exp(phi)
         dvidx = e/vi - vi/self.l
-        return [dphidx, nd2phidx2, dvidx]
+        return [dphidx, dedx, dvidx]
     
+    # Calculate the current.
     def current(self, phi):
         return self.cnt * np.exp(phi) - 1
 
 def run(system):
     
-    def grids(alpha, minor = 1): # Grid function.
+    # Grid function.
+    def grids(alpha, minor = 0): 
         plt.grid(b = True, which = "major", linestyle = "--", alpha = alpha)
-        if minor == 1:
+        if minor != 0:
             plt.grid(b = True, which = "minor", linestyle = "-.", alpha = alpha/4)
     
     # Close all figures.
@@ -51,26 +66,38 @@ def run(system):
     plt.rc("text", usetex = True)
     plt.rc("font", family = "serif", size = 16)
     
-    viwall = np.array([])
-    L = np.logspace(-1,4,6)
-    fig = plt.figure(1, figsize = (13,13/1.618)) # Create figure.
-    x = np.linspace(0., 100., 1000) # Create linspace.
-    for l in L: # Loop over vs
-        system = sheath([0., 0.001, 1.], 1., l) # Initialise system with desired vs.
-        f = sc.integrate.odeint(system, system.f0, x) # Integrate
+    # Create figure.
+    fig = plt.figure(1, figsize = (13,13/1.618))
+    # Linspace for x.
+    x = np.linspace(0., 100., 1000)
+    # Initialise empty array for vi @ wall.
+    vi_wall = np.array([])
+    # Logarithmic collision length array.
+    l_arr = np.logspace(-1,4,6)
+    # Loop over l_arr
+    for l in l_arr:
+        # Initialise system with desired l.
+        system = sheath([0., 0.001, 1.], 1., l)
+        # Integrate
+        f = sc.integrate.odeint(system, system.f0, x)
+        # Calculate J.
         j = system.current(f[:,0])
-        xwall = x - np.interp(0., j[::-1], x[::-1]) # Interpolate to find x when j = 0.
-        viwall = np.append(viwall, np.interp(0., xwall, f[:,2])) # Interpolate to find vi when xwall = 0.
-        plt.plot(xwall, f[:,2], label = (r"$\hat{L} = %.1f$" % l)) # Plot each curve
+        # Interpolate to find x when j = 0.
+        x_wall = x - np.interp(0., j[::-1], x[::-1])
+        # Interpolate to find vi when x_wall = 0.
+        # Append value to vi_wall array.
+        vi_wall = np.append(vi_wall, np.interp(0., x_wall, f[:,2]))
+        # Plot each curve.
+        plt.plot(x_wall, f[:,2], label = (r"$\hat{L} = %.1f$" % l))
         
-    grids(0.5, 0.0)
+    grids(0.5)
     plt.ylabel(r"Normalised Ion Velocity")
     plt.xlabel(r"Debye Lengths")
     plt.xlim([-20, 40]) ; plt.ylim([0, 5])
     plt.legend(loc=1)    
     
     ax = fig.add_axes([.125, .55, .15, .35])
-    ax.plot(L, viwall)
+    ax.plot(l_arr, vi_wall)
     ax.grid(alpha = 0.5)
     ax.set_ylabel(r"Ion Velocity @ Wall, $v_{i}(x = 0)$")
     ax.set_xlabel(r"Collision Length, L")
@@ -78,5 +105,5 @@ def run(system):
     plt.tight_layout()
     plt.savefig(filename = "collisions.eps", format = "eps")
 
-system = sheath([0.,0.001, 1.], 1., 0.1)
+system = sheath([0.,0., 0.], 0., 0.)
 run(system)
